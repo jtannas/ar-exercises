a59cc266c32d5d2e993e99694543b86c186d8b50
Joel Tannas <jtannas@gmail.com>
2018-02-27 15:26:49 -0700

    correct allowed salary range

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,5 +1,5 @@
 class Employee < ActiveRecord::Base
   validates_presence_of :first_name, :last_name, :store
-  validates :hourly_rate, :inclusion => {:in => 0..60}
+  validates :hourly_rate, :inclusion => {:in => 40..200}
   belongs_to :store
 end
